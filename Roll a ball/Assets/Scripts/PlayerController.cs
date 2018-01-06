1855721592b71e93e1e27d03f9b1f652126f821f
kamilciruspawlak <31177093+kamilciruspawlak@users.noreply.github.com>
2018-01-06 13:34:02 +0100

    first commit

---
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    private int points;
+    public Text pointCounter;
+    public Text gameState;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetPointsText();
+        gameState.text = "";
+       
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            points += 1;
+            SetPointsText();
+        }
+    }
+
+    void SetPointsText()
+    {
+        pointCounter.text = "Your point's : " + points.ToString();
+        if (points >= 12)
+        {
+            gameState.text = "Wygrałeś !";
+        }
+    }
+}
---
+++ Roll a ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Rotator : MonoBehaviour 
+    {
+        private void Update()
+        {
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }
+    }
+}
